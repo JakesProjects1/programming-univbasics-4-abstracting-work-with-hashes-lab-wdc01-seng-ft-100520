f3eac86686a1c7f945a32dfe3b705a27610bdbe8
Jacob Kagon <jacob@kagon.net>
2020-09-15 17:37:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,6 +2,7 @@ def my_hash_creator(key, value)
   {key: value
   }
   # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator(:name, "Grace Hopper")
 end
 
 def read_from_hash(hash, key)
